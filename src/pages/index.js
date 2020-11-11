b9a3a69529d764db1fcd11a5cf480b44542c0700
Albert Zhang <albertzhang@college.harvard.edu>
2020-11-11 12:37:36 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Home() {
-  return <div>Hello world!</div>
+import moon from '../img/moon.png'
+import galaxy from './galaxy.png'
+
+const HomePage = () => {
+  return (
+    <div class='container'>
+      <div class='row align-items-center'>
+        <div class='col'>
+          <h1>test</h1>
+        </div>
+        <div class='col'>
+          <img src={galaxy}/>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default HomePage
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
